2beb6631421940df9009f65e6608d250bf5b53d7
yassine hamdis <163686588+Yassine-Hamdis@users.noreply.github.com>
2026-01-17 16:17:54 +0100

    Enable DNS hostnames for VPC

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ variable private_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
